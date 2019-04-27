8f02af3976fb04cd3ddc700b135e5cc2858e6b3f
zartos <daikerumaru@hotmail.com>
2019-04-26 23:18:33 -0500

    Se añadio funcionalidad de buscar en el mapa de google

--- app/src/main/java/com/example/take_a_pic/MainActivity.java
+++ app/src/main/java/com/example/take_a_pic/MainActivity.java
@@ -2,32 +2,52 @@ package com.example.take_a_pic;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     public static final int REQUEST_CAPTURE_PHOTO = 120;
-    private ImageView iPhoto;
-
+    ImageView iPhoto;
+    Button bCamera;
+    Button bMap;
+    TextView sMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bCamera = findViewById(R.id.bCamera);
+        bCamera = findViewById(R.id.bCamera);
         iPhoto = findViewById(R.id.iPhoto);
-        bCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        bMap = findViewById(R.id.bMap);
+        sMap = findViewById(R.id.sMap);
+
+        bCamera.setOnClickListener(this);
+        bMap.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.bCamera:
                 takePhoto();
-            }
-        });
+                break;
+
+            case R.id.bMap:
+                String search = sMap.getText().toString();
+                Uri location = Uri.parse("geo:0,0?q=" + search );
+                lookMap(location);
+                break;
+                
+        }
 
     }
 
@@ -37,6 +57,14 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(intent, REQUEST_CAPTURE_PHOTO);
         }
     }
+
+    private void lookMap(Uri geoLocation){
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, geoLocation);
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+        }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
